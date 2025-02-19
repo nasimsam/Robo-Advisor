98a785612659facbc80bb9f231dae6a9cd9bb0ae
Nasim Samei <114447943+nasimsam@users.noreply.github.com>
2025-02-19 15:28:56 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import openai
+st.title("Robo Advisor")
+user_input= ['' for i in range(8)]
+user_input[0] = st.text_input("Please enter your name:")
+user_input[1] = st.text_input("Please enter your age:")
+user_input[2] = st.text_input("Please enter your income:")
+user_input[3] = st.text_input("Please enter your rent/morgage amount:")
+user_input[4] = st.selectbox("Please enter your investment horizon:", ["Short term", "Medium term", "Long term"])
+user_input[5] = st.text_input("Please enter your investment amount:")
+user_input[6] = st.text_input("Please enter your location:")
+user_input[7] = st.text_input("Please enter your investment goal:")
+
+#api = st.secrets["openai"]
+api ='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'
+def advise(input):
+    client = openai.OpenAI(api_key= api)
+    response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+           
+            {"role": "user", "content": f"Please provide a {input[4]} fincial advice for {input[0]} who is {input[1]} years old: given following information: the annaul income is {input[2]}, the monthly rent/morgage is {input[3]}, {input[0]} is living in {input[6]} and {input[0]} has the following investment goals: planning to invest {input[5]} annaully and {input[7]}"}
+        ]
+    )
+    
+    advise = response.choices[0].message.content
+    return advise
+    
+
+# Creating a button
+
+if st.button("Submit"):
+    print(user_input)
+
+    st.write("\n", advise(user_input))
+    
